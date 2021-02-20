a1eafad9c1c6ae82392247a34d036e127f724b19
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-02-20 13:16:40 +0700

    initial commit

---
+++ src/uas/if1/pkg10119033/dandiahmadin/pkg3/UASIF110119033DANDIAHMADIN3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if1.pkg10119033.dandiahmadin.pkg3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dandi Ahmadin
+ */
+public class UASIF110119033DANDIAHMADIN3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Masukan Umur Anda : ");
+        int umur = scan.nextInt();
+        boolean dewasa = false;
+        
+        if(umur >= 17) {
+            dewasa = true;
+        }
+        
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+    
+}
